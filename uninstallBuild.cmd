1f35475b28dcaefa21dbfd2ad538d46e1d319ec4
sucaba <wareverbohdan@gmail.com>
2013-08-27 01:42:01 +0300

    Removed not used files

--- uninstallBuild.cmd
+++
@@ -1,8 +0,0 @@
-set MSBuildExtensionsDir=%ProgramFiles(x86)%\MSBuild\
-set MSBuildCommonDir=%MSBuildExtensionsDir%4.0\Microsoft.Common.Targets\
-set IronTextDir=%MSBuildExtensionsDir%IronText\
-
-rmdir /q /s "%IronTextDir%"
-
-del "%MSBuildCommonDir%ImportBefore\IronText.ImportBefore.Common.props"
-del "%MSBuildCommonDir%ImportAfter\IronText.ImportAfter.Common.targets"
