11a0bff30fbab3c093e78146192973eec40e85cb
sucaba <wareverbohdan@gmail.com>
2013-08-28 14:30:50 +0300

    Refactoring extensibility

---
+++ Src/IronText.Compiler/MetadataCompiler/LanguageData.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using IronText.Algorithm;
+using IronText.Framework;
+using System.Collections.ObjectModel;
+using IronText.Extensibility;
+
+namespace IronText.MetadataCompiler
+{
+    /// <summary>
+    /// Precompiled language data
+    /// </summary>
+    internal class LanguageData : IReportData
+    {
+        public LanguageName Name { get; set; }
+
+        public BnfGrammar Grammar { get; set; }
+
+        public int TokenCount { get { return Lalr1ParserActionTable.ColumnCount; } }
+
+        ReadOnlyCollection<ScanMode> IReportData.ScanModes
+        {
+            get { return new ReadOnlyCollection<ScanMode>(ScanModes); }
+        }
+
+        ReadOnlyCollection<DotState> IReportData.ParserStates
+        {
+            get { return new ReadOnlyCollection<DotState>(ParserStates); }
+        }
+
+        ITdfaData IReportData.GetScanModeDfa(Type scanModeType)
+        {
+            return ScanModeTypeToDfa[scanModeType];
+        }
+
+        ParserAction IReportData.GetParserAction(int state, int token)
+        {
+            return ParserAction.Decode(Lalr1ParserActionTable.Get(state, token));
+        }
+
+        IEnumerable<ParserAction> IReportData.GetAllParserActions(int state, int token)
+        {
+            var cell = Lalr1ParserActionTable.Get(state, token);
+            var action = ParserAction.Decode(cell);
+            if (action != null && action.Kind == ParserActionKind.Conflict)
+            {
+                for (int i = 0; i != action.Size; ++i)
+                {
+                    yield return
+                        ParserAction.Decode(
+                            Lalr1ParserConflictActionTable[action.Value1 + i]);
+                }
+            }
+        }
+
+        ReadOnlyCollection<ParserConflictInfo> IReportData.GetParserConflicts()
+        {
+            var resultList = new List<ParserConflictInfo>();
+            for (int state = 0; state != ParserStates.Length; ++state)
+            {
+                for (int token = 0; token != Grammar.TokenCount; ++token)
+                {
+                    var cell = Lalr1ParserActionTable.Get(state, token);
+                    var action = ParserAction.Decode(cell);
+                    if (action != null && action.Kind == ParserActionKind.Conflict)
+                    {
+                        var item = new ParserConflictInfo
+                            {
+                                State = state,
+                                Token = token,
+                            };
+
+                        for (int i = 0; i != action.Size; ++i)
+                        {
+                            item.Actions.Add(
+                                ParserAction.Decode(
+                                    Lalr1ParserConflictActionTable[action.Value1 + i]));
+                        }
+
+                        resultList.Add(item);
+                    }
+                }
+            }
+
+            return new ReadOnlyCollection<ParserConflictInfo>(resultList);
+        }
+
+        IEnumerable<ParserAction> IReportData.GetConflictActions(int conflictIndex, int count)
+        {
+            for (int i = 0; i != count; ++i)
+            {
+                yield return ParserAction.Decode(
+                    Lalr1ParserConflictActionTable[conflictIndex + i]);
+            }
+        }
+
+        private ParserConflictInfo[] GetParserConflicts(IReportData data)
+        {
+            var resultList = new List<ParserConflictInfo>();
+            for (int state = 0; state != ParserStates.Length; ++state)
+            {
+                for (int token = 0; token != Grammar.TokenCount; ++token)
+                {
+                    var cell = Lalr1ParserActionTable.Get(state, token);
+                    var action = ParserAction.Decode(cell);
+                    if (action != null && action.Kind == ParserActionKind.Conflict)
+                    {
+                        var item = new ParserConflictInfo
+                            {
+                                State = state,
+                                Token = token,
+                            };
+
+                        for (int i = 0; i != action.Size; ++i)
+                        {
+                            item.Actions.Add(
+                                ParserAction.Decode(
+                                    Lalr1ParserConflictActionTable[action.Value1 + i]));
+                        }
+
+                        resultList.Add(item);
+                    }
+                }
+            }
+
+            return resultList.ToArray();
+        }
+
+        public bool                   IsAmbiguous;
+        public Type                   RootContextType;
+
+        public DotState[]             ParserStates;
+
+        internal ITokenRefResolver    TokenRefResolver; 
+
+        // Rule ID -> ActionBuilder
+        public GrammarActionBuilder[][] RuleActionBuilders;
+        public MergeRule[]            MergeRules;
+        public SwitchRule[]           SwitchRules;
+        public LocalParseContext[]    LocalParseContexts;
+        
+        public ScanMode[]             ScanModes;
+        public Dictionary<Type, ITdfaData>  ScanModeTypeToDfa;
+
+        public ITable<int>            ParserActionTable;
+        public int[]                  ParserConflictActionTable;
+        public int[]                  StateToSymbolTable;
+
+        // For reporting
+        public ITable<int>            Lalr1ParserActionTable;
+        public int[]                  Lalr1ParserConflictActionTable;
+
+        public string GetDestinationDirectory()
+        {
+            string result = Name.SourceAssemblyDirectory;
+            return result;
+        }
+    }
+}
---
+++ Src/IronText.Core/Extensibility/IReportData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using IronText.Framework;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace IronText.Extensibility
+{
+    /// <summary>
+    /// Contract for the language's reporting data
+    /// </summary>
+    public interface IReportData
+    {
+        LanguageName Name { get; }
+
+        BnfGrammar Grammar { get; }
+
+        int TokenCount { get; }
+
+        ReadOnlyCollection<ScanMode> ScanModes { get; }
+
+        ITdfaData GetScanModeDfa(Type scanModeType);
+
+        ReadOnlyCollection<DotState> ParserStates { get; }
+
+        string GetDestinationDirectory();
+
+        ParserAction GetParserAction(int state, int token);
+
+        IEnumerable<ParserAction> GetAllParserActions(int state, int token);
+
+        ReadOnlyCollection<ParserConflictInfo> GetParserConflicts();
+
+        IEnumerable<ParserAction> GetConflictActions(int conflictIndex, int count);
+    }
+}
