456f8ceee00231de816a279ed4083bb734f77b9d
sucaba <wareverbohdan@gmail.com>
2013-09-10 16:13:13 +0300

    Fixed incorrect git ignore pattern. Added missing cecil sources

---
+++ Deps/cecil/symbols/pdb/Microsoft.Cci.Pdb/SourceLocationProvider.cs
@@ -0,0 +1,26 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+
+namespace Microsoft.Cci {
+
+  internal sealed class PdbIteratorScope : ILocalScope {
+
+    internal PdbIteratorScope(uint offset, uint length) {
+      this.offset = offset;
+      this.length = length;
+    }
+
+    public uint Offset {
+      get { return this.offset; }
+    }
+    uint offset;
+
+    public uint Length {
+      get { return this.length; }
+    }
+    uint length;
+  }
+}
