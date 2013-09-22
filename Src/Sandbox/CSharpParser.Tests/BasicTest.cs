c09efe5f93e4249614aee89263f304800a794f52
sucaba <wareverbohdan@gmail.com>
2013-09-22 20:34:21 +0300

    Moved CSharpParser from Samples to Sandbox

---
+++ Src/Sandbox/CSharpParser.Tests/BasicTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronText.Framework;
+using NUnit.Framework;
+
+namespace CSharpParser.Tests
+{
+    [TestFixture]
+    [Explicit]
+    public class BasicTest
+    {
+        [Test]
+        public void Test()
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            string path = "Sample1.cs";
+            using (var interp = new Interpreter<ICsGrammar>())
+            {
+                TestLoad(timer, path, interp);
+
+                //int tokenCount = interp.Scan(input, path).Count();
+                //Console.WriteLine("token count = {0}", tokenCount);
+                //for (int i = 0; i != 10; ++i)
+                {
+                    timer.Reset();
+                    using (var input = new StreamReader(path))
+                    {
+                        timer.Start();
+                        interp.Parse(input, path);
+                        timer.Stop();
+                    }
+
+                    Console.WriteLine(
+                        "{1}(1,1): message : Parsing time={0}sec",
+                        timer.Elapsed.TotalSeconds,
+                        path);
+                }
+            }
+        }
+
+        private static void TestLoad(Stopwatch timer, string path, Interpreter<ICsGrammar> interp)
+        {
+            interp.LogKind = LoggingKind.ConsoleOut;
+            interp.Parse("/*a*/[assembly:Foo]");
+            timer.Stop();
+            if (timer.ElapsedMilliseconds > 1000)
+            {
+                Console.WriteLine(
+                    "{1}(1,1): warning : Language loading is too slow time={0}sec",
+                    timer.Elapsed.TotalSeconds,
+                    path);
+            }
+        }
+    }
+}
---
+++ Src/Sandbox/CSharpParser.Tests/Sample1.cs
@@ -0,0 +1,99 @@
+/*
+using System;
+using System.Collections.Generic;
+using IronText.Framework;
+
+namespace Calculator
+{
+
+    [Language]
+    [Precedence]
+*/
+    [Precedence(   1   )]
+    [Precedence("=", 0, Associativity.None)]
+    [Precedence("+", 1)]
+    [Precedence("*", 2)]
+    [Precedence("/", 2)]
+    [Precedence("%", 2)]
+    [Precedence("^", 10, Associativity.Right)]
+    // [DescribeParserStateMachine("Calculator.info")]
+    // [ParserGraph("Calculator.gv")]
+    public class Calculator
+    {
+        const string _ = null;
+
+        public readonly Dictionary<string,double> Variables = new Dictionary<string,double>();
+
+        [ParseResult]
+        public double Result { get; set; }
+
+        public bool Done { get; set; }
+
+        [Parse]
+        public double Number(Const<double> c) { return c == null ? 0 : c.Value; }
+
+        [Parse]
+        public double VarRef(string name) { return Variables[name]; }
+
+        [Parse(_, "+", _)]
+        public double Plus(double x, double y) { return  x + y; }
+        
+        [Parse(_, "-", _)]
+        public double Minus(double x, double y) { return  x - y; }
+
+        [Parse(_, "*", _)]
+        public double Prod(double x, double y) { return  x * y; }
+
+        [Parse(_, "/", _)]
+        public double Div(double x, double y) { return  x / y; }
+
+        [Parse(_, "%", _)]
+        public double Mod(double x, double y) { return  x % y; }
+
+        [Parse(_, "^", _)]
+        public double Pow(double x, double y) { return  Math.Pow(x, y); }
+
+        [Parse("sqrt", "(", _, ")")]
+        public double Sqrt(double x) { return Math.Sqrt(x); }
+
+        [Parse("sin", "(", _, ")")]
+        public double Sin(double x) { return Math.Sin(x); }
+
+        [Parse("cos", "(", _, ")")]
+        public double Cos(double x) { return Math.Cos(x); }
+
+        [Parse(_, "=", _)]
+        public double Let(string var, double rexpr) { Variables[var] = rexpr; return 0; }
+
+        [Parse("print", "(", _, ")")]
+        public double Print(double expr) { Console.WriteLine(expr); return 0; }
+
+        [Parse("exit")]
+        [Parse("quit")]
+        public void Exit() { Done = true; }
+
+        [Scan("blank+")]
+        public void Space() { }
+
+        [Scan("alpha alnum*")]
+        public string Identifier(string name) { return name; }
+
+        [Scan("digit+ ('.' digit*)?")]
+        public Const<double> Number(string text)
+        { 
+            return new Const<double>(double.Parse(text));
+        }
+
+        public class Const<T> 
+        {
+            public readonly T Value;
+
+            public Const(T value) { Value = value; }
+
+            public override string ToString()
+            {
+                return Value.ToString();
+            }
+        }
+    }
+//}
