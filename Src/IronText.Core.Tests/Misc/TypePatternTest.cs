4a3c957410181deb75424cea6e752914d25b4af4
sucaba <wareverbohdan@gmail.com>
2013-10-30 03:19:35 +0200

    Added TypePattern for type pattern matching

---
+++ Src/IronText.Core.Tests/Misc/TypePatternTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronText.Misc;
+using NUnit.Framework;
+
+namespace IronText.Tests.Misc
+{
+    [TestFixture]
+    public class TypePatternTest
+    {
+        [Test]
+        public void PatternMatchesType()
+        {
+            var matcher = typeof(IRules).GetMethod("Rule1");
+            var target = new TypePattern(matcher);
+
+            {
+                var type = typeof(Dictionary<Dictionary<int, string>, Dictionary<int, byte>>);
+                Type[] types = target.Match(type);
+                Assert.AreEqual(new[] { typeof(int), typeof(string), typeof(byte) }, types);
+                Assert.IsNotNull(target.MakeProducer(type));
+            }
+
+            {
+                // 2 types for the same type parameter
+                var type = typeof(Dictionary<Dictionary<int, string>, Dictionary<char, byte>>);
+                Type[] types = target.Match(type);
+                Assert.IsNull(types);
+                Assert.IsNull(target.MakeProducer(type));
+            }
+        }
+
+        interface IRules
+        {
+            Dictionary<Dictionary<T1, T2>, Dictionary<T1, T3>> Rule1<T1,T2,T3>();
+        }
+
+        public class Opt<T> { }
+    }
+}
---
+++ Src/IronText.Core/Misc/TypePattern.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace IronText.Misc
+{
+    internal class TypePattern
+    {
+        private readonly MethodInfo matcher;
+        private readonly Type patternType;
+        private readonly Type[] placeholders;
+
+        public TypePattern(MethodInfo matcher)
+        {
+            this.matcher = matcher;
+            this.patternType = matcher.ReturnType;
+
+            if (matcher.IsGenericMethodDefinition)
+            {
+                this.placeholders = matcher.GetGenericArguments();
+            }
+            else
+            {
+                this.placeholders = Type.EmptyTypes;
+            }
+        }
+
+        public Type[] Match(Type type)
+        {
+            Type[] result = null;
+            if (!Match(patternType, type, ref result))
+            {
+                return null;
+            }
+
+            return result;
+        }
+
+        public MethodInfo MakeProducer(Type resultType)
+        {
+            var types = Match(resultType);
+            if (types == null)
+            {
+                return null;
+            }
+
+            return matcher.MakeGenericMethod(types);
+        }
+
+        private bool Match(Type patternType, Type instanceType, ref Type[] output)
+        {
+            if (patternType.IsGenericType)
+            {
+                var patternGenericDef = patternType.GetGenericTypeDefinition();
+                var genericDef = patternType.GetGenericTypeDefinition();
+                if (!patternGenericDef.Equals(genericDef))
+                {
+                    return false;
+                }
+
+                var patternTypeArgs = patternType.GetGenericArguments();
+                var instanceTypeArgs = instanceType.GetGenericArguments();
+                for (int i = 0; i != instanceTypeArgs.Length; ++i)
+                {
+                    if (!Match(patternTypeArgs[i], instanceTypeArgs[i], ref output))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            if (patternType.IsGenericParameter)
+            {
+                int index = Array.IndexOf(placeholders, patternType);
+                if (index < 0)
+                {
+                    throw new InvalidOperationException("Internal error");
+                }
+
+                if (output == null)
+                {
+                    output = new Type[placeholders.Length];
+                }
+                
+                if (output[index] == null)
+                {
+                    output[index] = instanceType;
+                }
+                else if (!output[index].Equals(instanceType))
+                {
+                    // different types for single type-placeholder
+                    return false;
+                }
+
+                return true;
+            }
+
+            return patternType.Equals(instanceType);
+        }
+    }
+}
