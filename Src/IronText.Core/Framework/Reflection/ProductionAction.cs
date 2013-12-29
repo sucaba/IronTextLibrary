98f95bcead59467a5e63334f5b2fd7b69e007767
sucaba <wareverbohdan@gmail.com>
2013-12-30 00:59:06 +0200

    ProductionAction semantics change

--- Src/IronText.Core/Framework/Reflection/ProductionAction.cs
+++ Src/IronText.Core/Framework/Reflection/ProductionAction.cs
@@ -7,21 +7,33 @@ using IronText.Framework.Collections;
 
 namespace IronText.Framework.Reflection
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// Action applicatin invariant: 1 == inputSize + sum(1 - actionLen)
+    /// Refactor to hierarhy:
+    /// - ProductionActionBase
+    /// - SimpleProductionAction - single reduce aciton
+    /// - CompositeProductionAction - multi-reduce action. has multiple simple actions
+    /// </remarks>
     public class ProductionAction : IndexableObject<IEbnfContext>, ICloneable
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="start">Zero-based index relative to a production start</param>
-        /// <param name="argumentCount">Count of arguments consumed by the action</param>
-        public ProductionAction(int start, int argumentCount)
+        public ProductionAction(int backOffset)
+            : this(backOffset, backOffset)
         {
-            this.Start         = start;
+        }
+
+        /// <param name="backOffset">Zero-based index relative to a production end</param>
+        /// <param name="argumentCount">Count of arguments consumed by the action</param>
+        public ProductionAction(int backOffset, int argumentCount)
+        {
+            this.BackOffset    = backOffset;
             this.ArgumentCount = argumentCount;
             this.Bindings      = new Collection<IProductionActionBinding>();
         }
 
-        public int Start { get; private set; }
+        public int BackOffset { get; private set; }
 
         public int ArgumentCount { get; private set; }
 
@@ -29,7 +41,7 @@ namespace IronText.Framework.Reflection
 
         public ProductionAction Clone()
         {
-            var result = new ProductionAction(Start, ArgumentCount);
+            var result = new ProductionAction(BackOffset, ArgumentCount);
             foreach (var binding in Bindings)
             {
                 result.Bindings.Add(binding);
