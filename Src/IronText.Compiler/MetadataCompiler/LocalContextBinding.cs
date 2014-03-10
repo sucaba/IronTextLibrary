eca515d27b2a4c68d9bcbac118f2c42f4904ca6c
sucaba <wareverbohdan@gmail.com>
2014-03-11 01:18:37 +0200

    Code cleanup

--- Src/IronText.Compiler/MetadataCompiler/LocalContextBinding.cs
+++ Src/IronText.Compiler/MetadataCompiler/LocalContextBinding.cs
@@ -7,15 +7,15 @@ namespace IronText.Extensibility
         /// <summary>
         /// ID of the parent state
         /// </summary>
-        public int               StackState    { get; set; }
+        public int StackState    { get; set; }
 
         /// <summary>
         /// Tail relative position of the context token in stack
         /// </summary>
-        public int               StackLookback { get; set; }
+        public int StackLookback { get; set; }
 
-        public ActionContextProvider   Provider      { get; set; }
+        public ActionContextProvider Provider      { get; set; }
 
-        public ActionContextRef  Consumer      { get; set; }
+        public ActionContextRef      Consumer      { get; set; }
     }
 }
