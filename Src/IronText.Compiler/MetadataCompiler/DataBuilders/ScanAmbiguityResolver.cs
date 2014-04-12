4ca0e3fc922b84a9b813e25113c116c4702ddc9e
sucaba <wareverbohdan@gmail.com>
2014-04-12 13:28:12 +0300

    Made TokenProducerInfo class inner class of ScanAmbiguityResolver

--- Src/IronText.Compiler/MetadataCompiler/DataBuilders/ScanAmbiguityResolver.cs
+++ Src/IronText.Compiler/MetadataCompiler/DataBuilders/ScanAmbiguityResolver.cs
@@ -61,7 +61,6 @@ namespace IronText.MetadataCompiler
                         RealActions     = SparseIntSetType.Instance.Of(matcher.Index),
                         PossibleTokens  = tokenSetType.Empty
                     };
-            
             }
             else if ((ambiguous = outcome as AmbiguousSymbol) != null)
             {
@@ -129,6 +128,103 @@ namespace IronText.MetadataCompiler
 #if DEBUG
                 Debug.WriteLine("Created ambiguous symbol {0} for state #{1}", ambSymbol, prod.State.Index);
 #endif
+            }
+        }
+
+        class TokenProducerInfo
+        {
+            public TokenProducerInfo()
+            {
+                MainTokenId = PredefinedTokens.NoToken;
+            }
+
+            public TdfaState State { get; set; }
+
+            public Disambiguation Disambiguation { get; set; }
+
+            /// <summary>
+            /// The most probable token id or <c>-1</c> if there is no token to distinguish.
+            /// </summary>
+            public int MainTokenId { get; set; }
+
+            /// <summary>
+            /// All tokens which can be produced by this rule.
+            /// Can be empty for void-rules.
+            /// </summary>
+            public IntSet PossibleTokens { get; set; }
+
+            public IntSet RealActions { get; set; }
+
+            public override int GetHashCode()
+            {
+                return unchecked(MainTokenId + PossibleTokens.Count + RealActions.Count);
+            }
+
+            public override bool Equals(object obj)
+            {
+                return Equals(obj as TokenProducerInfo);
+            }
+
+            public bool Equals(TokenProducerInfo other)
+            {
+                return other != null
+                    && MainTokenId == other.MainTokenId
+                    && PossibleTokens.SetEquals(other.PossibleTokens)
+                    && RealActions.SetEquals(other.RealActions);
+            }
+
+            public static TokenProducerInfo Combine(IntSetType tokenSetType, IEnumerable<TokenProducerInfo> items)
+            {
+                switch (items.Count())
+                {
+                    case 0: return
+                            new TokenProducerInfo
+                            {
+                                Disambiguation = Disambiguation.Exclusive,
+                                RealActions = SparseIntSetType.Instance.Empty,
+                                PossibleTokens = tokenSetType.Empty,
+                                MainTokenId = -1,
+                            };
+                    case 1: return items.First();
+                }
+
+                TokenProducerInfo result = null;
+
+                foreach (var item in items)
+                {
+                    if (item.Disambiguation == Disambiguation.Exclusive)
+                    {
+                        if (result == null)
+                        {
+                            result = item;
+                        }
+                    }
+                }
+
+                if (result == null)
+                {
+                    result = new TokenProducerInfo { Disambiguation = Disambiguation.Exclusive };
+                    int mainToken = PredefinedTokens.NoToken;
+                    var allPossible = tokenSetType.Mutable();
+                    var allActions = SparseIntSetType.Instance.Mutable();
+
+                    foreach (var item in items)
+                    {
+                        if (mainToken != PredefinedTokens.NoToken && item.MainTokenId != PredefinedTokens.NoToken)
+                        {
+                            mainToken = item.MainTokenId;
+                        }
+
+                        allPossible.AddAll(item.PossibleTokens);
+                        allActions.AddAll(item.RealActions);
+                    }
+
+                    result.MainTokenId = mainToken;
+                    result.RealActions = allActions.CompleteAndDestroy();
+                    result.PossibleTokens = allPossible.CompleteAndDestroy();
+                }
+
+                return result;
             }
         }
     }
