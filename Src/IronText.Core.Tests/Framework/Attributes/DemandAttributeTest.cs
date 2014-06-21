05b1d8001f37404c76dbe43093ea6d42a4169b17
sucaba <wareverbohdan@gmail.com>
2014-06-22 01:44:30 +0300

    Missing file

---
+++ Src/IronText.Core.Tests/Framework/Attributes/DemandAttributeTest.cs
@@ -0,0 +1,63 @@
+﻿using IronText.Framework;
+using IronText.Runtime;
+using NUnit.Framework;
+
+namespace IronText.Tests.Framework.Attributes
+{
+    /// <summary>
+    /// A test class for <see cref="DemandAttribute"/>
+    /// </summary>
+    [TestFixture]
+    public class DemandAttributeTest
+    {
+        [Test]
+        public void InterfaceAsRootContext()
+        {
+            string input = "3 add 100 remove 10 add 1 end";
+            double result = Language.Parse(new RootContext(), input).Result;
+            Assert.AreEqual(94, result);
+        }
+
+        [Language]
+        public class RootContext
+        {
+            public double Result { get; [Produce] set; }
+
+            [Produce]
+            public Accumulator Seed(int initial) { return new Accumulator(initial); }
+
+            [Match("digit+")]
+            public int Number(string text) { return int.Parse(text); }
+
+            [Match("blank+")]
+            public void Space() { }
+        }
+
+        [Demand]
+        public class Accumulator
+        {
+            private  int value;
+
+            public Accumulator() : this(0) { }
+
+            public Accumulator(int initial)
+            {
+                value = initial;
+            }
+
+            [Produce("add")]
+            public Accumulator Add(int x) { value += x; return this; }
+
+            [Produce("remove")]
+            public Accumulator Remove(int x) { value -= x; return this; }
+
+            public interface Start { }
+
+            [Produce("end")]
+            public double End() { return value; }
+
+            //[Pattern("end")]
+            //public Start Close() { return default(Start); }
+        }
+    }
+}
