4b29dd64a01541d6ff141c421f92369942c0e441
sucaba <wareverbohdan@gmail.com>
2016-10-15 08:50:48 +0300

    Code cleanup

--- Src/IronText.Compiler/Automata/Lalr1/Lalr1DfaProvider.cs
+++ Src/IronText.Compiler/Automata/Lalr1/Lalr1DfaProvider.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using IronText.Algorithm;
 using IronText.Compiler.Analysis;
-using IronText.Reflection;
 using IronText.Runtime;
 using System.Linq;
 
